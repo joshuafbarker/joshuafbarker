076153241afa4ccca265e9d6e102ab6c953dcc2a
Joshua Barker <jobarker@redventures.com>
2022-01-13 21:53:42 -0500

    adds favicon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
+import Head from 'next/head';
 import 'styles/global.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <main>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        {/* page title */}
+        <title>jb.</title>
+        
+        {/* favicon */}
+        <link rel="shortcut icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👋</text></svg>" />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
